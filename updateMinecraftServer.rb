50bd3b68043758f5dce9ad5550d2e3da6e732993
Mike Burns <meburns90@gmail.com>
2012-05-22 13:20:28 -0500

    Added more verbose information for updateMinecraftServer.rb and fixed some broken code :)

--- updateMinecraftServer.rb
+++ updateMinecraftServer.rb
@@ -1,21 +1,26 @@
 def updateServer(a)
 
   b = Hash.new
+
+  # Turn minecraft's server.properties into a hash.
   File.open("server.properties").each { |line|
-    c,d = line.split("=")
-    b[c] = d
+    unless line.match(/^\#/)
+      c,d = line.split("=")
+      b[c] = d
+    end
   }
 
-  printf(b[white-list])
-
+  # Merge the new hash and the original server.properties hash.
   a.each { |key, value|
     b[key] = value
   }
 
+  # Recreate the server.properties file.
   text = ""
   b.each_key { |key|
     text += "#{key}=#{b[key]}"
   }
 
+  # Overwrite the old server.properties with the newly merged one.
   File.open("server.properties", "w") { |f| f.puts text }
 end
