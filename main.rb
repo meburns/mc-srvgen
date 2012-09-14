b9be04bdcf955a3dd7d122d3cd5b6cf651ac0d4b
Mike Burns <meburns90@gmail.com>
2012-09-14 12:25:02 -0500

    Created main file to run code
    The main file should be used to call all of the code that resides in
    other files.

---
+++ main.rb
@@ -0,0 +1,8 @@
+# main.rb
+# by meburns - 9/14/2012
+require 'createMinecraftServer'
+
+# run all the main code to generate a new minecraft server
+makeServerDirectory()
+getMinecraftServer()
+initMinecraftServer()
